7ce4f62afe1060b919c9771ce74602ace69923f2
khadidja-kebaili <80825550+khadidja-kebaili@users.noreply.github.com>
2022-05-06 12:36:36 +0200

    Create TinyTuya.py

---
+++ backend/TinyTuya.py
@@ -0,0 +1,12 @@
+# Zum Ausprobieren:
+
+import tinytuya
+d = tinytuya.OutletDevice('bf1562e54d66eccf59oq7t',
+                          '192.168.2.106', '1877eea950b2d4ff')
+
+d.set_version(3.3)
+'''d_status = d.status()
+print(d_status)'''
+
+d.set_value(2, 90)
+d.set_value(1,'open')
