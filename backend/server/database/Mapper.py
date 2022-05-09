f88c0c2bdaee6fa8c293d19494bea5421b22721f
khadidja-kebaili <80825550+khadidja-kebaili@users.noreply.github.com>
2022-05-09 15:10:10 +0200

    Mapper Ergänzungen

--- backend/server/database/Mapper.py
+++ backend/server/database/Mapper.py
@@ -11,23 +11,15 @@ class Mapper (AbstractContextManager, ABC):
         self._cnx = None
 
     def __enter__(self):
-        """Was soll geschehen, wenn wir beginnen, mit dem Mapper zu arbeiten?"""
-
-        """Wir testen, ob der Code im Kontext der lokalen Entwicklungsumgebung oder in der Cloud ausgeführt wird.
+        """Check ob der Code im Kontext der lokalen Entwicklungsumgebung oder in der Cloud ausgeführt wird.
         Dies ist erforderlich, da die Modalitäten für den Verbindungsaufbau mit der Datenbank kontextabhängig sind."""
 
         if os.getenv('GAE_ENV', '').startswith('standard'):
-            """Landen wir in diesem Zweig, so haben wir festgestellt, dass der Code in der Cloud abläuft.
-            Die App befindet sich somit im **Production Mode** und zwar im *Standard Environment*.
-            Hierbei handelt es sich also um die Verbindung zwischen Google App Engine und Cloud SQL."""
 
             self._cnx = connector.connect(user='demo', password='demo',
                                           unix_socket='',
                                           database='devicemanagement')
         else:
-            """Wenn wir hier ankommen, dann handelt sich offenbar um die Ausführung des Codes in einer lokalen Umgebung,
-            also auf einem Local Development Server. Hierbei stellen wir eine einfache Verbindung zu einer lokal
-            installierten mySQL-Datenbank her."""
 
             self._cnx = connector.connect(user='', password='',
                                           host='',
--- backend/server/database/ThermostatMapper.py
+++ backend/server/database/ThermostatMapper.py
@@ -1,7 +1,102 @@
 from .Mapper import Mapper
+from ..Thermostat import Thermostat
 
 
 class ThermostatMapper(Mapper):
 
     def __init__(self):
         super().__init__()
+
+    def insert(self, thermostat):
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT MAX(id) AS maxid FROM demo.demo ")
+        tuples = cursor.fetchall()
+        for (maxid) in tuples:
+            if maxid[0] is not None:
+                thermostat.set_ain(maxid[0] + 1)
+
+            else:
+                """Wenn wir KEINE maximale ID feststellen konnten, dann gehen wir
+                davon aus, dass die Tabelle leer ist und wir mit der ID 1 beginnen können."""
+                thermostat.set_ain(1)
+
+        command = "INSERT INTO demo.demo (ain, port) VALUES (%s, %s)"
+        data = (
+            thermostat.get_ain(),
+            thermostat.get_port()
+        )
+
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
+        return thermostat
+
+    def find_all(self):
+
+        result = []
+        cursor = self._cnx.cursor()
+        command = "SELECT id, dateoflastchange, time FROM demo.demo"
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        for (id, port) in tuples:
+            thermostat = Thermostat()
+            thermostat.set_ain(id)
+            thermostat.set_port(port)
+            result.append(thermostat)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_ain(self, ain):
+        result = None
+
+        cursor = self._cnx.cursor()
+        command = "SELECT ain, port, time FROM demo.demo WHERE id={}".format(
+            ain)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            (ain, port) = tuples[0]
+            thermostat = Thermostat()
+            thermostat.set_ain(ain)
+            thermostat.set_port(port)
+            result = thermostat
+
+        except IndexError:
+            """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
+            keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
+            result = None
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def update(self, thermostat):
+        cursor = self._cnx.cursor()
+
+        command = "UPDATE demo.demo " + \
+            "SET x=%s WHERE id=%s"
+        data = (thermostat.get_port(),
+                thermostat.get_ain())
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return thermostat
+
+    def delete(self, thermostat):
+        cursor = self._cnx.cursor()
+
+        command = "DELETE FROM demo.demo WHERE id={}".format(
+            thermostat.get_ain())
+        cursor.execute(command)
+
+        self._cnx.commit()
+        cursor.close()
