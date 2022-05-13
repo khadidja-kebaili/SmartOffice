53ffb9c6cade6f78e356cb78d9548b3a98bf061f
MartinaRutsch <martinarutsch94@gmail.com>
2022-05-13 11:33:30 +0200

    Kommentare

--- frontend/webapp/controller/Thermo-Main.controller.js
+++ frontend/webapp/controller/Thermo-Main.controller.js
@@ -13,18 +13,17 @@ sap.ui.define([
 
 		handleOpenDialog: function() {
 			var oView = this.getView();
-
-            if (!this._pDialog) {
-				this._pDialog = Fragment.load({
+            if (!this._pDialog) {													// Abfrage ob Dialog im Fragment schon existiert
+				this._pDialog = Fragment.load({										// Dialog laden
 					id: oView.getId(),
 					name: "com.quanto.solutions.ui.smartoffice.view.Dialog",
 					controller: this
-				}).then(function(oDialog) {
-					oView.addDependent(oDialog);
+				}).then(function(oDialog) {											 
+					oView.addDependent(oDialog);									// Abhängigkeit herstellen
 					return oDialog;
 				});
 			}
-			this._pDialog.then(function(oDialog){
+			this._pDialog.then(function(oDialog){									
 				oDialog.setModel(oView.getModel());
 				oDialog.open();
 			});
