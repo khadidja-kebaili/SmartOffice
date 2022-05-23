3013dd4f915206ee7a54225b8b8bd24679b1c1be
Hannah Hielscher <hh057@hdm-stuttgart.de>
2022-05-23 12:55:36 +0000

    set up branch

--- frontend/webapp/controller/Jalousien.controller.js
+++ frontend/webapp/controller/Jalousien.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
+    'sap/m/MessageToast',
     "sap/ui/core/mvc/Controller"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (MessageToast, Controller) {
         "use strict";
 
         return Controller.extend("com.quanto.solutions.ui.smartoffice.controller.Jalousien", {
@@ -14,6 +15,10 @@ sap.ui.define([
             handleNavButtonPress: function (evt) {
                 var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                 oRouter.navTo("home");
+            },
+            onLiveChange: function (oEvent) {
+                var sNewValue = oEvent.getParameter("value");
+                this.byId("getValue").setText(sNewValue);
             }
         });
     });
--- frontend/webapp/controller/SmartOffice.controller.js
+++ frontend/webapp/controller/SmartOffice.controller.js
@@ -9,7 +9,7 @@ sap.ui.define([
 
         return Controller.extend("com.quanto.solutions.ui.smartoffice.controller.SmartOffice", {
             onInit: function () {
-
+                
             }
         });
     });
