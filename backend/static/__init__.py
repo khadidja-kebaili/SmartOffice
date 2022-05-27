9eeb7619d59c0e5df090fd5aee41f937437ade28
khadidja-kebaili <80825550+khadidja-kebaili@users.noreply.github.com>
2022-05-27 10:32:32 +0200

    Same

--- backend/static/__init__.py
+++ backend/static/__init__.py
@@ -1,3 +1,4 @@
 from flask import Flask
-app = Flask(__name__, template_folder='static.templates')
+app = Flask(__name__, template_folder='templates')
+
 import backend.static.routes
--- backend/static/routes.py
+++ backend/static/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template, jsonify
+from flask_restx import Resource, Api, fields
 import time
 from datetime import datetime
 from . import app
+
 
 
 @app.route('/')
@@ -11,6 +13,30 @@ def index():
 
 @app.route('/DashboardAnalytics', methods=["GET"])
 def Test_migration():
+    """
+    Return a simple odata container with date time information
+    :return:
+    """
+    odata = {
+        'd': {
+            'results': []
+        }
+    }
+    i = 0
+    names = 'abcdefghijklmnopqrxtu'
+    while i < 20:
+        odata['d']['results'].append({
+            "id": i,
+            "name": names[i],
+            "datetime": datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'),
+
+        })
+        i += 1
+
+    return jsonify(odata)
+
+@app.route('/DashboardAnalyticszzz', methods=["GET"])
+def Test_migrationzzz():
     """
     Return a simple odata container with date time information
     :return:
