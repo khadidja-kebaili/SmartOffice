165ccfff16e494d802f1eb5c58cd5bd2ad259468
Hannah Hielscher <hh057@hdm-stuttgart.de>
2022-06-06 12:49:09 +0200

    connection with get and post

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -3,6 +3,7 @@ from flask_restx import Resource, Api, fields
 import time
 from datetime import datetime
 from flaskr import app
+from flask import request
 
 
 @app.route('/')
@@ -31,4 +32,13 @@ def Test():
         i += 1
 
     return jsonify(odata)
-    
+
+@app.route('/Jalousien', methods=["POST"])
+def TestJalousien():
+    """
+    Return a simple odata container with date time information
+    :return:
+    """
+    data = request.form["value"]
+    print(data)
+    return data
