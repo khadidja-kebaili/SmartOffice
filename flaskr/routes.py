365c09636987999c94cc7927b4082972796b5be0
khadidja-kebaili <80825550+khadidja-kebaili@users.noreply.github.com>
2022-06-13 14:35:00 +0200

    Update routes.py
    main Methoden für Frontend

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,9 +1,8 @@
 from flask import render_template, jsonify
-from flask_restx import Resource, Api, fields
-import time
-from datetime import datetime
 from flaskr import app
 from flask import request
+from flaskr.server.DeviceAdministration import DeviceAdministration
+import time
 
 
 @app.route('/')
@@ -12,33 +11,198 @@ def index():
 
 
 @app.route('/Test', methods=["GET"])
-def Test():
+#hier ist die get_Status Methode
+#former Test()
+def Status():
     """
     Return a simple odata container with date time information
     :return:
     """
+    adm = DeviceAdministration()
+
     odata = {
         'd': {
             'results': []
         }
     }
     i = 0
-    names = 'abcdefghijklmnopqrxtu'
-    while i < 20:
+    names = adm.get_all_status()
+
+    for elem in names:
         odata['d']['results'].append({
-            "id": i,
-            "name": names[i]
+            'date': elem.get_date(),
+            'percentage': elem.get_percentage()
         })
-        i += 1
 
     return jsonify(odata)
 
-@app.route('/Jalousien', methods=["POST"])
-def TestJalousien():
+@app.route('/LastStatusJalousien', methods=["GET"])
+#hier ist die get_Status Methode
+def LastStatusJalousien():
     """
     Return a simple odata container with date time information
     :return:
     """
+    adm = DeviceAdministration()
+
+    odata = {
+        'd': {
+            'results': []
+        }
+    }
+
+    last_status = adm.get_last_status()
+
+    odata['d']['results'].append({
+        'date': last_status.get_date(),
+        'percentage': last_status.get_percentage()
+    })
+
+    return jsonify(odata)
+
+
+@app.route('/Jalousien', methods=["POST"])
+def set_jal():
+    """
+    Return a simple odata container with date time information
+    :return:
+    """
+
+    adm = DeviceAdministration()
+
     data = request.form["value"]
-    print(data)
+    time.sleep(4)
+    adm.set_status_to_percentage_by_id(1, data)
+    jal = adm.get_last_status()
+    print('Jal: ', jal)
     return data
+
+@app.route('/Status', methods=["GET"])
+def Status_by_Timeperiode():
+    """
+    Return a simple odata container with date time information
+    :return:
+    """
+    """
+    Return a simple odata container with date time information
+    :return:
+    """
+
+    adm = DeviceAdministration()
+
+    start = request.form["start"]
+    end = request.form["end"]
+    stats = adm.get_all_stats_by_timeperiod(start, end)
+    print('Jal: ', stats)
+
+    odata = {
+        'd': {
+            'results': []
+        }
+    }
+
+    for elem in stats:
+        odata['d']['results'].append({
+            'date': elem.get_date(),
+            'percentage': elem.get_percentage()
+        })
+
+    return odata
+
+@app.route('/Jalousien', methods=["POST"])
+def set_temp():
+    """
+    Return a simple odata container with date time information
+    :return:
+    """
+
+    adm = DeviceAdministration()
+
+    data = request.form["value"]
+    time.sleep(4)
+    adm.set_temperature(data)
+    temp = adm.get_temperature()
+    print('temp: ', temp)
+    return data
+
+@app.route('/LastStatusJalousien', methods=["GET"])
+#hier ist die get_Status Methode
+def get_temp():
+    """
+    Return a simple odata container with date time information
+    :return:
+    """
+    adm = DeviceAdministration()
+
+    odata = {
+        'd': {
+            'results': []
+        }
+    }
+
+    temp = adm.get_temperature()
+    odata['d']['results'].append({
+        'temperature': temp
+    })
+
+    return jsonify(odata)
+
+@app.route('/LastStatusJalousien', methods=["GET"])
+#hier ist die get_Status Methode
+def get_standard_weekly_plan():
+    """
+    Return a simple odata container with date time information
+    :return:
+    """
+    adm = DeviceAdministration()
+
+    odata = {
+        'd': {
+            'results': []
+        }
+    }
+
+    temp = adm.get_temperature()
+    odata['d']['results'].append({
+        'temperature': temp
+    })
+
+    return jsonify(odata)
+
+
+@app.route('/Jalousien', methods=["POST"])
+def set_min_temp():
+    """
+    Return a simple odata container with date time information
+    :return:
+    """
+
+    adm = DeviceAdministration()
+    data = request.form["value"]
+    adm.set_min_temp(data)
+    temp = adm.get_min_temp()
+    print('temp: ', temp)
+    return data
+
+@app.route('/LastStatusJalousien', methods=["GET"])
+#hier ist die get_Status Methode
+def get_min_temp():
+    """
+    Return a simple odata container with date time information
+    :return:
+    """
+    adm = DeviceAdministration()
+
+    odata = {
+        'd': {
+            'results': []
+        }
+    }
+
+    temp = adm.get_min_temp()
+    odata['d']['results'].append({
+        'min_temperature': temp
+    })
+
+    return jsonify(odata)
+
