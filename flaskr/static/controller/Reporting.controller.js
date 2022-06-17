bf311abaef3259700ff1f0f4bc718d06fdf63dfd
Maria Weinberger <65721399+mariaweinberger@users.noreply.github.com>
2022-06-17 21:48:29 +0200

    Ergänzung Reporting

--- flaskr/static/controller/Reporting.controller.js
+++ flaskr/static/controller/Reporting.controller.js
@@ -10,10 +10,37 @@ sap.ui.define([
 
         return SmartOfficeController.extend("com.quanto.solutions.ui.smartoffice.controller.Reporting",{
             onInit: function () {
-                this.oModelSettings = new JSONModel;
+                this.oModelSettings = new JSONModel({
+                    maxIterations: 200,
+                    maxTime: 500,
+                    initialTemperature: 200,
+                    coolDownStep: 1
+                });
                 this.getView().setModel(this.oModelSettings, "settings");
                 this.getView().setModel(sap.ui.getCore().getModel("TestModel"), "TestModel");
+                this.getView().setModel(this.oModelSettings, "percentage");
                 sap.ui.core.BusyIndicator.hide(0);
-            }
+            },
+            getValue: function(oEvent) {
+                console.log("Werte werden geladen.");
+                sap.ui.core.BusyIndicator.hide(0);
+                //var oThis = this;
+                var oData = {
+                    'percentage': oEvent.getParameter("percentage")
+                };
+                console.log(oData),
+                jQuery.ajax({
+                    url : "/LastStatusJalousien",
+                    type : "GET",
+                    dataType : "json",
+                    async : true,
+                    data : oData,
+                    success : function(response){
+                        MessageToast.show(response.data.message);
+                        //oThis.makeGraph(response.graph);
+                        sap.ui.core.BusyIndicator.hide();
+                    },
+                    error: function(response){
+                        console.log(response);
     });
 });
