698ff6d916e095e231046e8b9fadb188f51b32fd
khadidja-kebaili <80825550+khadidja-kebaili@users.noreply.github.com>
2022-06-19 17:04:14 +0200

    Aktuelle Dumps

---
+++ flaskr/server/database/Dump20220619/smartoffice_monday.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: smartoffice
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `monday`
+--
+
+DROP TABLE IF EXISTS `monday`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `monday` (
+  `id` int NOT NULL,
+  `type` varchar(45) NOT NULL,
+  `start_time` varchar(128) DEFAULT NULL,
+  `end_time` varchar(128) DEFAULT NULL,
+  `value` int DEFAULT NULL,
+  PRIMARY KEY (`id`,`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `monday`
+--
+
+LOCK TABLES `monday` WRITE;
+/*!40000 ALTER TABLE `monday` DISABLE KEYS */;
+INSERT INTO `monday` VALUES (1,'J','09:00:00','11:00:00',20),(2,'T','09:00:00','12:00:00',260),(3,'J','09:00:00','11:00:00',20),(4,'J','09:00:00','11:00:00',20),(5,'J','09:00:00','11:00:00',20),(6,'T','09:00:00','11:00:00',240),(7,'T','09:00:00','11:00:00',240),(8,'T','09:00:00','11:00:00',240),(9,'T','09:00:00','11:00:00',240),(10,'T','13:00:00','14:00:00',280),(11,'T','13:00:00','14:00:00',280),(12,'T','13:00:00','14:00:00',280),(13,'T','13:00:00','14:00:00',280),(14,'T','13:00:00','14:00:00',280),(15,'T','13:00:00','14:00:00',280),(16,'T','13:00:00','14:00:00',280),(17,'T','13:00:00','14:00:00',280),(18,'T','13:00:00','14:00:00',280),(19,'T','13:00:00','14:00:00',280),(20,'T','13:00:00','14:00:00',280),(21,'T','13:00:00','14:00:00',280),(22,'T','13:00:00','14:00:00',280),(23,'J','09:00:00','14:00:00',95),(24,'T','09:00:00','14:00:00',95),(25,'J','09:00:00','14:00:00',95),(26,'T','09:00:00','14:00:00',95),(27,'J','09:00:00','14:00:00',95),(28,'T','09:00:00','14:00:00',95);
+/*!40000 ALTER TABLE `monday` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-19 17:03:48
