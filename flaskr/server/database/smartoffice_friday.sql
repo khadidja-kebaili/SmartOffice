4ca84fe161e6fb228487cf846debc8a7079bb4dc
khadidja-kebaili <80825550+khadidja-kebaili@users.noreply.github.com>
2022-06-20 12:31:37 +0200

    Dump Dateien für die neuen ThermostatStatusBOs und Mapper

---
+++ flaskr/server/database/smartoffice_friday.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: smartoffice
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `friday`
+--
+
+DROP TABLE IF EXISTS `friday`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `friday` (
+  `id` int NOT NULL,
+  `type` varchar(45) NOT NULL,
+  `start_time` varchar(128) DEFAULT NULL,
+  `end_time` varchar(128) DEFAULT NULL,
+  `value` int DEFAULT NULL,
+  PRIMARY KEY (`id`,`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `friday`
+--
+
+LOCK TABLES `friday` WRITE;
+/*!40000 ALTER TABLE `friday` DISABLE KEYS */;
+INSERT INTO `friday` VALUES (1,'J','09:00:00','11:00:00',20),(3,'J','09:00:00','14:00:00',95),(4,'J','09:00:00','14:00:00',95),(5,'J','09:00:00','14:00:00',95),(6,'T','09:00:00','14:00:00',95),(9,'J','11:00:00','13:00:00',70),(10,'J','11:00:00','13:00:00',70),(11,'J','11:00:00','13:00:00',70),(12,'J','11:00:00','13:00:00',80),(39,'T','09:00:00','11:00:00',260),(40,'T','09:00:00','11:00:00',260),(41,'T','16:00:00','17:00:00',230),(42,'T','15:00:00','16:00:00',230),(43,'T','19:00:00','20:30:00',200);
+/*!40000 ALTER TABLE `friday` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-20 12:30:33
---
+++ flaskr/server/database/smartoffice_standard_temp.sql
@@ -0,0 +1,65 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: smartoffice
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `standard_temp`
+--
+
+DROP TABLE IF EXISTS `standard_temp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `standard_temp` (
+  `id` int NOT NULL,
+  `weekday` int DEFAULT NULL,
+  `monday_id` int DEFAULT NULL,
+  `tuesday_id` int DEFAULT NULL,
+  `wednesday_id` int DEFAULT NULL,
+  `thursday_id` int DEFAULT NULL,
+  `friday_id` int DEFAULT NULL,
+  KEY `friday_id_idx` (`friday_id`),
+  KEY `monday_id_idx` (`monday_id`),
+  KEY `wednesday_id_idx` (`wednesday_id`),
+  KEY `thursday_id_idx` (`thursday_id`),
+  KEY `tuesday_id_temp_idx` (`tuesday_id`),
+  CONSTRAINT `friday_id_temp` FOREIGN KEY (`friday_id`) REFERENCES `friday` (`id`),
+  CONSTRAINT `monday_id_temp` FOREIGN KEY (`monday_id`) REFERENCES `monday` (`id`),
+  CONSTRAINT `thursday_id_temp` FOREIGN KEY (`thursday_id`) REFERENCES `thursday` (`id`),
+  CONSTRAINT `tuesday_id_temp` FOREIGN KEY (`tuesday_id`) REFERENCES `tuesday` (`id`),
+  CONSTRAINT `wednesday_id_temp` FOREIGN KEY (`wednesday_id`) REFERENCES `wednesday` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `standard_temp`
+--
+
+LOCK TABLES `standard_temp` WRITE;
+/*!40000 ALTER TABLE `standard_temp` DISABLE KEYS */;
+INSERT INTO `standard_temp` VALUES (1,NULL,22,NULL,NULL,NULL,NULL),(2,NULL,24,NULL,NULL,NULL,NULL),(3,NULL,26,NULL,NULL,NULL,NULL),(4,NULL,NULL,6,NULL,NULL,NULL),(5,NULL,28,NULL,NULL,NULL,NULL),(6,NULL,NULL,8,NULL,NULL,NULL),(7,NULL,NULL,NULL,7,NULL,NULL),(8,NULL,NULL,NULL,NULL,6,NULL),(9,NULL,NULL,NULL,NULL,NULL,6),(10,5,NULL,NULL,NULL,NULL,40),(11,5,NULL,NULL,NULL,NULL,41),(12,5,NULL,NULL,NULL,NULL,42),(13,5,NULL,NULL,NULL,NULL,43);
+/*!40000 ALTER TABLE `standard_temp` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-20 12:30:31
