df82461b6f2f3b66bf2bb702c71465d58dc94f04
khadidja-kebaili <80825550+khadidja-kebaili@users.noreply.github.com>
2022-06-25 12:30:50 +0200

    httpS eingefügt und funktioniert.

--- main.py
+++ main.py
@@ -32,19 +32,29 @@ def check_jal_stat(thread_name, delay):
     date = datetime.now()
     weekday = date.isoweekday()
     adm = DeviceAdministration()
+    stats = []
     if weekday == 1:
-        stats = adm.get_all_jal_standard_entries_monday()
+        entries = adm.get_all_jal_standard_entries_monday()
+        for elem in entries:
+            stats.append(elem)
     elif weekday == 2:
-        stats = adm.get_all_jal_standard_entries_tuesday()
+        entries = adm.get_all_jal_standard_entries_tuesday()
+        for elem in entries:
+            stats.append(elem)
     elif weekday == 3:
-        stats = adm.get_all_jal_standard_entries_wednesday()
+        entries = adm.get_all_jal_standard_entries_wednesday()
+        for elem in entries:
+            stats.append(elem)
     elif weekday == 4:
-        stats = adm.get_all_jal_standard_entries_thursday()
+        entries = adm.get_all_jal_standard_entries_thursday()
+        for elem in entries:
+            stats.append(elem)
     elif weekday == 5:
-        stats = adm.get_all_jal_standard_entries_friday()
+        entries = adm.get_all_jal_standard_entries_friday()
+        for elem in entries:
+            stats.append(elem)
     else:
         print('It´s the weekend!')
-
     count = True
     while count:
         for elem in stats:
@@ -62,16 +72,27 @@ def check_temp_stat(thread_name, delay):
     date = datetime.now()
     weekday = date.isoweekday()
     adm = DeviceAdministration()
+    stats = []
     if weekday == 1:
-        stats = adm.get_all_temp_standard_entries_monday()
+        entries = adm.get_all_temp_standard_entries_monday()
+        for elem in entries:
+            stats.append(elem)
     elif weekday == 2:
-        stats = adm.get_all_temp_standard_entries_tuesday()
+        entries = adm.get_all_temp_standard_entries_tuesday()
+        for elem in entries:
+            stats.append(elem)
     elif weekday == 3:
-        stats = adm.get_all_temp_standard_entries_wednesday()
+        entries = adm.get_all_temp_standard_entries_wednesday()
+        for elem in entries:
+            stats.append(elem)
     elif weekday == 4:
-        stats = adm.get_all_temp_standard_entries_thursday()
+        entries = adm.get_all_temp_standard_entries_thursday()
+        for elem in entries:
+            stats.append(elem)
     elif weekday == 5:
-        stats = adm.get_all_temp_standard_entries_friday()
+        entries = adm.get_all_temp_standard_entries_friday()
+        for elem in entries:
+            stats.append(elem)
     else:
         print('It´s the weekend!')
 
@@ -90,7 +111,7 @@ def check_temp_stat(thread_name, delay):
 
 def something():
     if __name__ == "__main__":
-        app.run(debug=False)
+        app.run('0.0.0.0', debug=False, port=8100, ssl_context='adhoc')
 
 
 # Create new threads
